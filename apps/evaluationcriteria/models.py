00102a14ebfcad5ca51edc9b5ee1ac94a3e112b8
gift-mercy <giftmercy81730@gmail.com>
2026-04-10 08:53:29 +0300

    updated project

--- apps/evaluationcriteria/models.py
+++ apps/evaluationcriteria/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class evaluationcriteria(models.Model):
-    
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank = True)
+    max_score = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.name
+
+
 
