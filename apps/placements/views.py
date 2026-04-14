b817d6093a2d1948383ba2a27d36773d9e1b382a
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-14 14:28:09 +0300

    Update views.py

--- apps/placements/views.py
+++ apps/placements/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+from .models import InternshipPlacement
+from .serializers import InternshipPlacementserializer# Create your views here.
