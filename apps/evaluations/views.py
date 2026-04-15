787ec7448c08731fca5c7263dd52e3720816cd9d
gift-mercy <giftmercy81730@gmail.com>
2026-04-15 11:01:50 +0300

    updated serialzers

--- apps/evaluations/views.py
+++ apps/evaluations/views.py
@@ -4,4 +4,5 @@ from .serializers import EvaluationSerializer
 
 class EvaluationViewSet(viewsets.ModelViewSet):
     queryset = Evaluation.objects.all()
-    
+    serializer_class = EvaluationSerializer
+
