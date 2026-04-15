9ff85e199e336d80c30fff96de15f174444bb7ff
gift-mercy <giftmercy81730@gmail.com>
2026-04-15 10:43:22 +0300

    added serializer

--- apps/evaluations/views.py
+++ apps/evaluations/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Evaluation
+from .serializers import EvaluationSerializer
+
 
 # Create your views here.
