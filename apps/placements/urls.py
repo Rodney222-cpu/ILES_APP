d7eff9777c4b175655ad2ee91f072bf9dbe9b3e8
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-16 20:27:41 +0300

    Setting urls.py for placements

--- apps/placements/urls.py
+++ apps/placements/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import InternshipPlacementViewSet
+
+router = DefaultRouter()
+router.register(r'internship', InternshipPlacementViewSet, basename = 'internship')
