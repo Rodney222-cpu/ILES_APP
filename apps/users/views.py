322cd8c762da287186cb78ac8bb339437ecba90a
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-22 16:33:27 +0300

    Edits made

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,13 @@ from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
 User = get_user_model()
 
 class UserViewSet (viewsets.ModelViewSet):
