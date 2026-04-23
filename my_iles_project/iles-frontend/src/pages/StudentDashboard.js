fc545a17aa216715539eedc8758e930c2e49eed5
gift-mercy <giftmercy81730@gmail.com>
2026-04-23 11:34:34 +0300

    updated frontend

--- my_iles_project/iles-frontend/src/pages/StudentDashboard.js
+++ my_iles_project/iles-frontend/src/pages/StudentDashboard.js
@@ -6,17 +6,17 @@ const StudentDashboard = () => {
         <div className = "dashboard">
             <h1>Student Dashboard</h1>
 
-            <div>
+            <div className = "card">
                 <h2>Welcome , Student</h2>
                 <p>Your internship details will appear here.</p>
             </div>
 
-            <div>
+            <div className = "card">
                 <h3>Internshp Status</h3>
                 <p>Status: Ongoing</p>
             </div>
 
-            <div>
+            <div className = "card">
                 <h3>Supervisor</h3>
                 <p>Name:</p>
             </div>
