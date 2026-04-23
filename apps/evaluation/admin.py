2985e937281b40f4bb275315916c5c6122985e65
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-23 12:28:21 +0300

    Update evaluation admin.py

--- apps/evaluation/admin.py
+++ apps/evaluation/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Evaluation
 
-# Register your models here.
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):# Register your models here.
