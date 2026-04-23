a5cb7c41d6a31cda4f5fb113ddc355bdec7105e9
gift-mercy <giftmercy81730@gmail.com>
2026-04-23 10:13:56 +0300

    updated project

--- my_iles_project/iles-frontend/src/pages/StudentDashboard.js
+++ my_iles_project/iles-frontend/src/pages/StudentDashboard.js
@@ -1,4 +1,23 @@
 import React from "react"
 
 const StudentDashboard = () => {
-    
+    return (
+        <div>
+            <h1>Student Dashboard</h1>
+
+            <div>
+                <h2>Welcome , Student</h2>
+                <p>Your internship details will appear here.</p>
+            </div>
+
+            <div>
+                <h3>Internshp Status</h3>
+                <p>Status: Ongoing</p>
+            </div>
+
+            <div>
+                <h3>Supervisor</h3>
+                <p>Name:</p>
+            </div>
+        </div>
+
