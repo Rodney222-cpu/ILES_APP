b64b22d7eda5e7ce5da219bbe37a2457ea4824ad
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-23 13:50:54 +0300

    edits made on login page

---
+++ my_iles_project/iles-frontend/src/pages/Login.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/login/",
+        {
+          username,
+          password,
+        }
+      );
+
+      const { token, role } = response.data;
+
+      // Save token & role
+      localStorage.setItem("token", token);
+      localStorage.setItem("role", role);
+
+      // Redirect based on role
+      if (role === "student") {
+        window.location.href = "/student-dashboard";
+      } else if (role === "admin") {
+        window.location.href = "/admin-dashboard";
+      } else if (role === "workplace_supervisor") {
+        window.location.href = "/workplace-dashboard";
+      } else if (role === "academic_supervisor") {
+        window.location.href = "/academic-dashboard";
+      }
+
+    } catch (error) {
+      alert("Invalid username or password");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
