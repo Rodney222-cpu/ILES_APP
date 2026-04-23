c6bb06cad6133e104282bd62aa3dedfb3c6267e1
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-23 13:34:34 +0300

    edits made

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, login_view
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),  
+    path('login/', login_view),       
+]
+
