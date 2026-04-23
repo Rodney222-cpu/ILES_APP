5f975c30f9a4b2044710957d6deb682af70064a3
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-23 12:14:55 +0300

    Update weekly log admin.py

--- apps/weekly_logs/admin.py
+++ apps/weekly_logs/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import WeeklyLog
 
 @admin.register(WeeklyLog)
-class WeeklyLogAdmin(admin.ModelAdmin):# Register your models here.
+class WeeklyLogAdmin(admin.ModelAdmin):
+    list_display = ('placement', 'week_number', 'submitted_at')
+    list_filter = ('submitted_at')# Register your models here.
