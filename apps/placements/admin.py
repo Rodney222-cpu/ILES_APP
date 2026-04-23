bce78560ea827ffe9bda3105568f8cdd8c163586
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-23 11:43:43 +0300

    Update placements admin.py

--- apps/placements/admin.py
+++ apps/placements/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import placements
 
 @admin.register(placements)
-class PlacementAdmin(admin.ModelAdmin):# Register your models here.
+class PlacementAdmin(admin.ModelAdmin):
+    list_display = ('student', 'company_name', 'status', 'start_date')
+    list_filter = ('status', 'company_name')
+    search_fields = ('student__username', 'company_name')# Register your models here.
