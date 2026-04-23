fe70cc298a50f156efd0c74d39f83c92dab63b3e
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-23 14:42:54 +0300

    edits made

--- my_iles_project/iles-frontend/src/App.js
+++ my_iles_project/iles-frontend/src/App.js
@@ -1,3 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
