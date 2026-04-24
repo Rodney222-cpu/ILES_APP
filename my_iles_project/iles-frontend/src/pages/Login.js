e14ba6fb2f788478f10d47f89a54fff7b83e6fcf
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-24 15:05:30 +0300

    Made edits

--- my_iles_project/iles-frontend/src/pages/Login.js
+++ my_iles_project/iles-frontend/src/pages/Login.js
@@ -48,4 +48,10 @@ function Login() {
           type="text"
           placeholder="Username"
           onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          style={styles.input}
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
         /> 
