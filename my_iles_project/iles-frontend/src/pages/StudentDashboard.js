d599e32c97dd4275009320c8256f89a8c5248afd
gift-mercy <giftmercy81730@gmail.com>
2026-04-25 11:43:56 +0300

    updated project

--- my_iles_project/iles-frontend/src/pages/StudentDashboard.js
+++ my_iles_project/iles-frontend/src/pages/StudentDashboard.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
-import React from "react";
+import axios from "axios";
 import Navbar from "../components/Navbar";
 import SummaryCard from "../components/SummaryCard";
 import LogsTable from "../components/LogsTable";
 import logs from "../data/dummyLogs";
 
 const StudentDashboard = () => {
+  const [student, setStudent] = useState(null);
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(true);
   return (
     <div>
       <Navbar />
 
       <div style={styles.container}>
         <h2>Student Dashboard</h2>
-        <h3>Welcome </h3>
+        <h3>Welcome {student?.name}</h3>
         <p>Your internship progress overview</p>
 
         {/* Summary Cards */}
