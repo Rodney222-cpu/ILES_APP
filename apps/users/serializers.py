3517505f7334e3050448d6b2990134200857e8d9
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-27 14:17:42 +0300

    Edits made

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -28,5 +28,17 @@ class UserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
                                                                                                                   
