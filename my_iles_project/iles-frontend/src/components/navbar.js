775f00f49bb153821349c4265d706dff1037b84b
gift-mercy <giftmercy81730@gmail.com>
2026-04-27 15:55:27 +0300

    changed codes

--- my_iles_project/iles-frontend/src/components/navbar.js
+++ my_iles_project/iles-frontend/src/components/navbar.js
@@ -1,8 +1,14 @@
 import React from "react"
+import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
-    <div style={styles.nav}>
-      <h2 style={styles.logo}>ILES (Internship Login and Evaluation System)</h2>
+    
+    <div style={styles.logoContainer}>
+      <img src={logo} alt="ILES Logo" style={styles.logoImage} />
+      <h2 style={styles.logoText}>
+        ILES (Internship Login and Evaluation System)
+      </h2>
+    </div>
 
       <div style={styles.links}>
         <button>Dashboard</button>
