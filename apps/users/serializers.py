c72d02a82ad759879b0008261b3ea67943318a8b
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-27 14:09:53 +0300

    Edits made

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,9 +18,11 @@ class UserSerializer(serializers.ModelSerializer):
             'student_number'
         ]
         extra_kwargs = {
-    'password': {'write_only': True}
-}
+            'password': {'write_only': True}
+        }
+
     def create(self, validated_data):
-         user = User.objects.create_user(**validated_data)
-         return user
-                                                                                                                      
+        user = User.objects.create_user(**validated_data)
+        return user
+
+                                                                                                                  
