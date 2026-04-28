77bcd8cb4b2282914a836eb0d8274588efc58e79
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-28 15:14:57 +0300

    Edits made

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, login_view
+from .views import register_view
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
