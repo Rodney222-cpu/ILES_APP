af255d3e3bbb410d85ce19377d84b808b11bfdea
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-28 15:27:14 +0300

    Edits made on login

--- my_iles_project/iles-frontend/src/pages/Login.js
+++ my_iles_project/iles-frontend/src/pages/Login.js
@@ -4,6 +4,7 @@ import axios from "axios";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async () => {
     try {
