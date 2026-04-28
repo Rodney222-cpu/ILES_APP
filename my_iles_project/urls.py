90536df811fc64bae75cbd27d3ebfe255bc42827
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-28 09:08:12 +0300

    Updated urls.py main

--- my_iles_project/urls.py
+++ my_iles_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Placement Management Admin"
+admin.site.site_title = "Placement Portal"
+admin.site.index_title = "Welcome to the Dashboard"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.placements.urls')),
