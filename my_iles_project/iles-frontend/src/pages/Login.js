42956d8c846b82b41a0865336512bcc6a1374248
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-28 15:56:51 +0300

    Edits made on login page

--- my_iles_project/iles-frontend/src/pages/Login.js
+++ my_iles_project/iles-frontend/src/pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 function Login() {
   const [username, setUsername] = useState("");
