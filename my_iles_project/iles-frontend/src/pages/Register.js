43c3569c5c8b64dd111944bced5cdf7eef57ffcc
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-29 19:10:46 +0300

    Edits made

--- my_iles_project/iles-frontend/src/pages/Register.js
+++ my_iles_project/iles-frontend/src/pages/Register.js
@@ -9,7 +9,7 @@ function Register() {
 
   const handleRegister = async () => {
       if (!username || !password || !confirmPassword) {
-           alert("Please enter your username and password");
+           alert("Please fill in all fields");
               return;
   }
 
