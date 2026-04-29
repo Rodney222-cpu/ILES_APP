1216cd3e14038c1a98b19a4fec9b8bde7015bc10
Rebecca Nabbanja <nabbanjarebecca9@gmail.com>
2026-04-29 13:28:53 +0300

    Edits made

--- my_iles_project/iles-frontend/src/pages/Login.js
+++ my_iles_project/iles-frontend/src/pages/Login.js
@@ -8,7 +8,11 @@ function Login() {
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleLogin = async () => {
+  const handleLogin = async () => { 
+     if (!username || !password) {
+    setError("All fields are required");
+    return;
+  }
     try {
       const response = await axios.post(
         "http://127.0.0.1:8000/api/login/",
