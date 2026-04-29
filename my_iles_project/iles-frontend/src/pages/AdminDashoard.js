c3af4dc7ee3b8e670b2612018c6b2dee8e1ab60c
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-29 22:45:52 +0300

    Set admindashboard

--- my_iles_project/iles-frontend/src/pages/AdminDashoard.js
+++ my_iles_project/iles-frontend/src/pages/AdminDashoard.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import api from './api';
 
 function Dashboard() {
     const [placements, setPlacements] = useState([]);
