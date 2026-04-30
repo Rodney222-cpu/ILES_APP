74da39db7c991da2791f023ed510be117a3149dc
baratuthulayyah <baratuthurayyah@gmail.com>
2026-04-30 21:18:23 +0300

    Update settings.py

--- my_iles_project/settings.py
+++ my_iles_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'dj_rest_auth',
     'corsheaders',
     'apps.users',
     'apps.weekly_logs',
