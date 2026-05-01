0f331748f35ea10438c6e4eb67332a07e396b6ce
gift-mercy <giftmercy81730@gmail.com>
2026-05-01 10:27:03 +0300

    edited code

--- apps/evaluation/admin.py
+++ apps/evaluation/admin.py
@@ -3,4 +3,4 @@ from .models import Evaluation
 
 @admin.register(Evaluation)
 class EvaluationAdmin(admin.ModelAdmin):
-    list_display = ( 'grade', 'created_at') # Register your models here.
+    list_display = [ 'grade', 'created_at'] # Register your models here.
