d7ed2c24df2104c9dea0d50668d11d48c1829412
gift-mercy <giftmercy81730@gmail.com>
2026-05-05 09:43:20 +0300

    made changes

--- my_iles_project/urls.py
+++ my_iles_project/urls.py
@@ -37,5 +37,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('evaluation/', include('apps.evaluation.urls')),
+    path('', include('evaluation.urls')),
 ]
+
